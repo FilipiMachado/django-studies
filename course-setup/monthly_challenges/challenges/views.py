3969a8ac9a1e53ea00a6352949efff9183b80eea
filipi <filipisee@hotmail.com>
2022-03-21 20:00:04 -0300

    dynamic function for requests

--- course-setup/monthly_challenges/challenges/views.py
+++ course-setup/monthly_challenges/challenges/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
-def index(request):
+def january(request):
     return HttpResponse("This works!")
+
+def february(request):
+    return HttpResponse("This works on February!")
+
+def march(request):
+    return HttpResponse("This is actually working on March too!")
+
+def monthly_challenges(request, month):
+    challenge_text = None
+    if month == 'january':
+        challenge_text = 'This works!'
+    elif month == 'february':
+        challenge_text = 'This works on February!'
+    elif month == 'march':
+        challenge_text = 'This is actually working on March too!'
+    else:
+        return 
+    return HttpResponse(challenge_text)
