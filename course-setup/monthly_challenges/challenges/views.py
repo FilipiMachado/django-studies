1e1a0c98495e9de2ff0257254d4c6f19ecb70bc7
filipi <filipisee@hotmail.com>
2022-03-22 10:36:59 -0300

    adding html

--- course-setup/monthly_challenges/challenges/views.py
+++ course-setup/monthly_challenges/challenges/views.py
@@ -20,6 +20,18 @@ monthly_challenges = {
 
 # Create your views here.
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+    
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+    
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
     
@@ -33,6 +45,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("This month is not valid!")
+        return HttpResponseNotFound("<h1>This month is not valid!</h1>")
