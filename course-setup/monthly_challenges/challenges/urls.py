6192b973490bb20f0033f812f9159e106e44999c
filipi <filipisee@hotmail.com>
2022-03-22 22:01:32 -0300

    registering templates

--- course-setup/monthly_challenges/challenges/urls.py
+++ course-setup/monthly_challenges/challenges/urls.py
@@ -7,3 +7,4 @@ urlpatterns = [
   path("<int:month>", views.monthly_challenge_by_number),
   path("<str:month>", views.monthly_challenge, name="month-challenge"),
 ]
+
--- course-setup/monthly_challenges/challenges/views.py
+++ course-setup/monthly_challenges/challenges/views.py
@@ -2,6 +2,7 @@ from turtle import forward
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 monthly_challenges = {
     "january": "This works",
@@ -45,7 +46,8 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = f"<h1>{challenge_text}</h1>"
+        response_data = render_to_string("challenges/challenge.html")
         return HttpResponse(response_data)
     except:
         return HttpResponseNotFound("<h1>This month is not valid!</h1>")
+    
