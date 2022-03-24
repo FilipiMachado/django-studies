e95786143c00c1b25f5ef3d016792735a9381153
filipi <filipisee@hotmail.com>
2022-03-23 23:09:06 -0300

    finishing 404 template

--- course-setup/monthly_challenges/challenges/views.py
+++ course-setup/monthly_challenges/challenges/views.py
@@ -1,8 +1,7 @@
 from turtle import forward
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-from django.template.loader import render_to_string
 
 monthly_challenges = {
     "january": "This works!",
@@ -46,6 +45,5 @@ def monthly_challenge(request, month):
             "title": month
         })
     except:
-        response_data = render_to_string("404.html")
-        return HttpResponseNotFound(response_data)
+        raise Http404()
     
